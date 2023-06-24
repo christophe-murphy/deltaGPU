19da3e438598598eb9d6c7047be1e500340daf3e
christophe-murphy <christophe.murphy1@jacobs.com>
2023-06-24 09:49:59 -0400

    Added camera and ray initialization

--- include/Camera.cuh
+++ include/Camera.cuh
@@ -2,6 +2,26 @@
 
 class camera {
     public:
-	__host__ __device__ Camera(const int px_width, const int px_height, const float3& posn,
-			           const float3& target, const float3& up);
+	__host__ __device__ Camera(const int px_width_in, const int px_height_in,
+			           const float3& posn_in, const float3& target, const float3& up);
 	__global__ float ray_trace(const Model& model, const int h_pixel, const int v_pixel);
+
+    private:
+	__device__ Ray start_ray(const int u_px, const int v_px) const;
+	int px_width;
+	int px_height;
+	float view_width;
+	float view_height;
+	float3 posn;
+	float3 top_left_pos;
+	float3 horizontal_dir;
+	float3 vertical_dir;
+	float3 forward_dir;
+	const float vfov = 40.0;
+};
+
+inline __device__ Ray Camera::start_ray(const int u_px, const int v_px) const {
+    float u = ((float) u_px + 0.5) / (float) px_width;
+    float v = ((float) v_px + 0.5) / (float) px_height;
+    return Ray(top_left_pos + horizontal_dir*u - vertical_dir*v, -forward_dir);
+}
--- src/Camera.cu
+++ src/Camera.cu
@@ -1,6 +1,19 @@
 #include "Camera.cuh"
 
-__host__ __device__ Camera(const int px_width, const int px_height, const float3& posn,
-		           const float3& target, const float3& up) {
-    
+__host__ __device__ Camera::Camera(const int px_width_in, const int px_height_in,
+		                   const float3& posn_in, const float3& target, const float3& up) {
+    float h = tan(degrees_to_radians(vfov));
+    view_height = 2 * h;
+    view_width = view_height * (float) px_width / (float) px_height;
+    posn = posn_in;
+    forward_dir = posn - target;
+    forward_dir *= rnorm3d(forward_dir);
+    horizontal_dir = cross(up, forward_dir);
+    horizontal_dir *= rnorm3d(horizontal_dir);
+    vertical_dir = cross(forward_dir, horizontal_dir);
+    horizontal_dir *= view_width;
+    vertical_dir *= view_height;
+    top_left_pos = posn - horizontal_dir/2 + vertical_dir/2 - forward_dir;
 }
+
+
